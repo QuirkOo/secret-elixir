bb6f6e9ee84a10343a52f91e19fc554bda9d93fe
Beata Obrok <beata@obrok.eu>
2015-01-11 19:21:59 +0100

    Add registry bucket spawning test

---
+++ mix_ex/test/registry_test.exs
@@ -0,0 +1,17 @@
+defmodule MixEx.RegistryTest do
+	use ExUnit.Case, async: true
+
+	setup do
+		{:ok, registry} = MixEx.Registry.start_link
+		{:ok, registry: registry}
+	end
+
+	test "registry is initially empty", %{registry: registry} do
+		assert MixEx.Registry.lookup(registry, "shopping") == :error
+	end
+
+	test "spawns buckets", %{registry: registry} do
+		MixEx.Registry.create(registry, "shopping")
+		assert {:ok, bucket} = MixEx.Registry.lookup(registry, "shopping")
+	end
+end
