637255ad638c4a8df53ffed26ec913e4cd89c975
Beata Obrok <beata@obrok.eu>
2015-01-12 14:32:03 +0100

    Registry events for creating and stopping buckets

--- mix_ex/test/registry_test.exs
+++ mix_ex/test/registry_test.exs
@@ -1,8 +1,20 @@
 defmodule MixEx.RegistryTest do
 	use ExUnit.Case, async: true
 
+	defmodule Forwarder do
+		use GenEvent
+
+		def handle_event(event, parent) do
+			send parent, event
+			{:ok, parent}
+		end
+	end
+
 	setup do
-		{:ok, registry} = MixEx.Registry.start_link
+		{:ok, manager} = GenEvent.start_link
+		{:ok, registry} = MixEx.Registry.start_link(manager)
+
+		GenEvent.add_mon_handler(manager, Forwarder, self())
 		{:ok, registry: registry}
 	end
 
@@ -20,5 +32,14 @@ defmodule MixEx.RegistryTest do
 		{:ok, bucket} = MixEx.Registry.lookup(registry, "shopping")
 		Agent.stop(bucket)
 		assert MixEx.Registry.lookup(registry, "shopping") == :error
+	end
+
+	test "sends events on create or crash", %{registry: registry} do
+		MixEx.Registry.create(registry, "shopping")
+		{:ok, bucket} = MixEx.Registry.lookup(registry, "shopping")
+		assert_receive {:create, "shopping", ^bucket}
+
+		Agent.stop(bucket)
+		assert_receive {:exit, "shopping", ^bucket}
 	end
 end
